214fd769b12962db41352942ec3ab4e4db8652b3
choijaewoo <cchoi00332244@gmail.com>
2024-11-19 20:55:08 +0900

    lab07

--- lab07/lab07/src/FileImporter.java
+++ lab07/lab07/src/FileImporter.java
@@ -1,3 +1,6 @@
-public interface FileImporter {
+import java.util.Map;
 
+public interface FileImporter<T> {
+    Map<String, T> importFile(String filepath);
+    void exportFile(String filepath, Map<String, T> map);
 }
